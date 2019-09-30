bee894f30f3849172851d0471d75420187e5708c
chengrongkai <crk@jianzhimao.com>
2019-09-30 10:20:27 +0800

    simple_01:两数之和

--- src/code_1/Code1.java
+++ src/code_1/Code1.java
@@ -12,7 +12,8 @@ public class Code1 {
         Solution solution = new Solution();
         int[] nums = {3,2,4};
         int target = 6;
-        int[] result = solution.twoSum(nums,target);
+//        int[] result = solution.twoSum(nums,target);
+        int[] result = solution.twoSumBest(nums,target);
         for (int i = 0; i < result.length; i++) {
             int i1 = result[i];
             System.out.println(i1);
@@ -40,5 +41,29 @@ class Solution {
             }
         }
         return result;
+    }
+
+    /**
+     * 核心思想：用额外的数组extra存储当前循环中的数据，每次遍历数据时，比较其和目标值的差值并在extra中进行查找，巧妙的利用值作为下标，类似于hash查找
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSumBest(int[] nums, int target) {
+        int indexArrayMax = 2047;
+        int[] indexArrays = new int[indexArrayMax + 1];
+        for (int i = 0; i < nums.length; i++) {
+            //获取差值
+            int diff = target - nums[i];
+            //避免负数
+            int index = diff & indexArrayMax;
+            //查看indexArray数组中是否以存储了这个值，如果存储了，则说明找到了可以累加得到target的值
+            if (indexArrays[index] != 0) {
+                return new int[] { indexArrays[index] - 1, i };
+            }
+            //将nums[i]作为下标存储i+1的值
+            indexArrays[nums[i] & indexArrayMax] = i + 1;
+        }
+        throw new IllegalArgumentException("No two sum value");
     }
 }
